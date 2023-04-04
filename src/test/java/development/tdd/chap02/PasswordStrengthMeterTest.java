471c00eac8e6452818d0e12f7591b0771304ee37
YunByungil <dbsquddlfz@naver.com>
2023-04-04 18:33:54 +0900

    암호 검사기 테스트 시작 (검증 없는 테스트 메서드 생성)

---
+++ src/test/java/development/tdd/chap02/PasswordStrengthMeterTest.java
@@ -0,0 +1,12 @@
+package development.tdd.chap02;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PasswordStrengthMeterTest {
+
+    @DisplayName("테스트 메서드 생성")
+    @Test
+    void name() {
+    }
+}
