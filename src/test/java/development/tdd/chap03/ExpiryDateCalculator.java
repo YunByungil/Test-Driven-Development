50e6779785b9c2b6eb0e65825d9e3d0751edfbee
YunByungil <dbsquddlfz@naver.com>
2023-04-17 21:13:35 +0900

    chapter3: 첫 납부일과 이후 납부일의 일자가 다른 경우를 처리하기 위한 코드 작성

--- src/test/java/development/tdd/chap03/ExpiryDateCalculator.java
+++ src/test/java/development/tdd/chap03/ExpiryDateCalculator.java
@@ -5,8 +5,11 @@ import java.time.LocalDate;
 public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(PayData payData) {
         if (payData.getFirstBillingDate() != null) {
-            if (payData.getFirstBillingDate().equals(LocalDate.of(2019, 1, 31))) {
-                return LocalDate.of(2019, 3, 31);
+            // 첫 납부일과 이후 납부일의 일자가 다른 경우를 처리하기 위한 코드
+
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(1); // 후보 만료일 구함
+            if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) { // 첫 납부일의 일자와 후보 만료일의 일자가 다르면
+                return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth()); // 첫 납부일의 일자를 후보 만료일의 일자로 사용
             }
         }
 
