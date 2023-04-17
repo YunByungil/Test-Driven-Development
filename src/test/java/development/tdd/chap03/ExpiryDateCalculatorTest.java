43a7c3e400f3fbfc9f2bcf97b227b716d27680cb
YunByungil <dbsquddlfz@naver.com>
2023-04-17 20:56:25 +0900

    chapter3: 테스트 코드가 assertExpiryDate() 메서드와 PayData 클래스 사용하도록 리팩토링

--- src/test/java/development/tdd/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/development/tdd/chap03/ExpiryDateCalculatorTest.java
@@ -25,13 +25,17 @@ public class ExpiryDateCalculatorTest {
     void 만원_납부하면_한달_뒤가_만료일이_됨() {
 
         assertExpiryDate(
-                LocalDate.of(2019, 3, 1),
-                10_000,
+                PayData.builder()
+                        .billingDate(LocalDate.of(2019, 3, 1))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2019, 4, 1));
 
         assertExpiryDate(
-                LocalDate.of(2019, 5, 5),
-                10_000,
+                PayData.builder()
+                        .billingDate(LocalDate.of(2019, 5, 5))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2019, 6, 5));
     }
 
@@ -45,20 +49,26 @@ public class ExpiryDateCalculatorTest {
     @Test
     void 납부일과_한달_뒤_일자가_같지_않음() {
         assertExpiryDate(
-                LocalDate.of(2019, 1, 31),
-                10_000,
-                LocalDate.of(2019, 2, 28)
+                PayData.builder()
+                        .billingDate(LocalDate.of(2019, 3, 1))
+                        .payAmount(10_000)
+                        .build(),
+                LocalDate.of(2019, 4, 1)
         );
 
         assertExpiryDate(
-                LocalDate.of(2019, 5, 31),
-                10_000,
+                PayData.builder()
+                        .billingDate(LocalDate.of(2019, 5, 31))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2019, 6, 30)
         );
 
         assertExpiryDate(
-                LocalDate.of(2020, 1, 31),
-                10_000,
+                PayData.builder()
+                        .billingDate(LocalDate.of(2020, 1, 31))
+                        .payAmount(10_000)
+                        .build(),
                 LocalDate.of(2020, 2, 29)
         );
     }
