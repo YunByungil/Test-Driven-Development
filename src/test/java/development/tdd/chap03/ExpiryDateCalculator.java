678b1a43d4f64b152ebf715585927f3d23ec347c
YunByungil <dbsquddlfz@naver.com>
2023-04-17 22:59:39 +0900

    chapter3: calculateExpiryDate 상수 변경 못한 거 다시 변경 완료

--- src/test/java/development/tdd/chap03/ExpiryDateCalculator.java
+++ src/test/java/development/tdd/chap03/ExpiryDateCalculator.java
@@ -8,8 +8,9 @@ public class ExpiryDateCalculator {
         if (payData.getFirstBillingDate() != null) {
             // 첫 납부일과 이후 납부일의 일자가 다른 경우를 처리하기 위한 코드
 
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(1); // 후보 만료일 구함
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths); // 후보 만료일 구함
             if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) { // 첫 납부일의 일자와 후보 만료일의 일자가 다르면
+                System.out.println("candidateExp = " + candidateExp);
                 return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth()); // 첫 납부일의 일자를 후보 만료일의 일자로 사용
             }
         }
