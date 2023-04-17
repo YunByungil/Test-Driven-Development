11c35510f62eca8a0e888241ca756c73d93feb22
YunByungil <dbsquddlfz@naver.com>
2023-04-17 23:06:30 +0900

    chpater3: 후보 만료일이 포함된 달의 마지막 날 < 첫 납부일의 일자 이 조건이 참이면 후보 만료일을 그달의 마지막 날로 조정

--- src/test/java/development/tdd/chap03/ExpiryDateCalculator.java
+++ src/test/java/development/tdd/chap03/ExpiryDateCalculator.java
@@ -1,6 +1,7 @@
 package development.tdd.chap03;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(PayData payData) {
@@ -10,6 +11,10 @@ public class ExpiryDateCalculator {
 
             LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths); // 후보 만료일 구함
             if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) { // 첫 납부일의 일자와 후보 만료일의 일자가 다르면
+                if (YearMonth.from(candidateExp).lengthOfMonth() < payData.getFirstBillingDate().getDayOfMonth()) {
+                    return candidateExp.withDayOfMonth(YearMonth.from(candidateExp).lengthOfMonth());
+                    // 후보 만료일이 포함된 달의 일자 길이에 따른 만료일 계산 로직 추가
+                }
                 System.out.println("candidateExp = " + candidateExp);
                 return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth()); // 첫 납부일의 일자를 후보 만료일의 일자로 사용
             }
