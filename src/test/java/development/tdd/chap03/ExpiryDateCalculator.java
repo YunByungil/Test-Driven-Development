9f91e647c760f61f3418ce17d7fc86997a35bbbe
YunByungil <dbsquddlfz@naver.com>
2023-04-17 23:16:41 +0900

    chapter3: 메서드 리팩토링 마무리

--- src/test/java/development/tdd/chap03/ExpiryDateCalculator.java
+++ src/test/java/development/tdd/chap03/ExpiryDateCalculator.java
@@ -9,19 +9,25 @@ public class ExpiryDateCalculator {
         if (payData.getFirstBillingDate() != null) {
             // 첫 납부일과 이후 납부일의 일자가 다른 경우를 처리하기 위한 코드
 
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths); // 후보 만료일 구함
-            final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
-            if (dayOfFirstBilling != candidateExp.getDayOfMonth()) { // 첫 납부일의 일자와 후보 만료일의 일자가 다르면
-                final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
-                if (dayLenOfCandiMon < dayOfFirstBilling) {
-                    return candidateExp.withDayOfMonth(dayLenOfCandiMon);
-                    // 후보 만료일이 포함된 달의 일자 길이에 따른 만료일 계산 로직 추가
-                }
-                System.out.println("candidateExp = " + candidateExp);
-                return candidateExp.withDayOfMonth(dayOfFirstBilling); // 첫 납부일의 일자를 후보 만료일의 일자로 사용
-            }
+            return expiryDateUsingFirstBillingDate(payData, addedMonths);
+        } else {
+            return payData.getBillingDate().plusMonths(addedMonths);
         }
+    }
 
-        return payData.getBillingDate().plusMonths(addedMonths);
+    private LocalDate expiryDateUsingFirstBillingDate(PayData payData, int addedMonths) {
+        LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths); // 후보 만료일 구함
+        final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+        if (dayOfFirstBilling != candidateExp.getDayOfMonth()) { // 첫 납부일의 일자와 후보 만료일의 일자가 다르면
+            final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
+            if (dayLenOfCandiMon < dayOfFirstBilling) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+                // 후보 만료일이 포함된 달의 일자 길이에 따른 만료일 계산 로직 추가
+            }
+            System.out.println("candidateExp = " + candidateExp);
+            return candidateExp.withDayOfMonth(dayOfFirstBilling);
+        } else {
+            return candidateExp;
+        }
     }
 }
