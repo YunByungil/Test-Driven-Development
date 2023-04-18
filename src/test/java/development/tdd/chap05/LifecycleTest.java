2bfccb97cfff56bf4d7a335c88c8ad06d32a7f99
YunByungil <dbsquddlfz@naver.com>
2023-04-18 13:00:41 +0900

    chapter5: 라이프사이클 관련 애노테이션 사용한 코드 작성 (BeforeEach, AfterEach)

---
+++ src/test/java/development/tdd/chap05/LifecycleTest.java
@@ -0,0 +1,34 @@
+package development.tdd.chap05;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LifecycleTest {
+    public LifecycleTest() {
+        System.out.println("new LifecycleTest");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("setUp");
+    }
+
+    @DisplayName("테스트1")
+    @Test
+    void a() {
+        System.out.println("A");
+    }
+
+    @DisplayName("테스트2")
+    @Test
+    void b() {
+        System.out.println("B");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("tearDown");
+    }
+}
